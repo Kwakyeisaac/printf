8eaf8d6692d14c40fe50275d44b76d963bd27d1d
bavly-adel <bavllyadell1998@gmail.com>
2023-07-24 08:59:58 -0700

    printf edit and new files for put

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.character strings
@@ -7,52 +6,32 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int k, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
-	if (format == NULL)
-		return (-1);
-	va_start(list, format);
-	for (k = 0; format && format[k] != '\0'; k++)
-	{
-		if (format[k] != '%')
-		{
-			buffer[buff_ind++] = format[k];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-				/* write(1, &format[k], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &k);
-			width = get_width(format, &k, list);
-			precision = get_precision(format, &k, list);
-			size = get_size(format, &k);
-			++k;
-			printed = handle_print(format, &k, list, buffer,
-					flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-	print_buffer(buffer, &buff_ind);
-	va_end(list);
-	return (printed_chars);
-}
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
+unsigned h = 0, r_value = 0;
+va_list args;
+va_start(args, format);
+for ( ; format[h] != '\0' ; h++)
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-	*buff_ind = 0;
+if (format[h] != '%')
+{
+putchr(format[h]);
 }
-
+else if (format[h+1] == 'c')
+{
+putchr(va_arg(args, int));
+h++;
+}
+else if (format[h+1] == 's')
+{
+int r_val = put_s(va_arg(args, char *));
+h++;
+r_value += (r_val - 1);
+}
+else if (format[h+1] == '%')
+{
+putchr('%');
+h++;
+}
+r_value += 1;
+}
+return (r_value);
+}
